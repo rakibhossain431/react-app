4cd84fc4a52a959d94cefbae3fc84673e4d24cbd
rakibhossain431 <rakibjp431@gmail.com>
2022-02-15 21:42:18 +0600

    react-app

---
+++ rakib-app/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>>MY name is rakib</>
+    </div>
+  );
+}
+
+export default App;
---
+++ rakib-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const todoapp = "This is javascript"
+
+ReactDOM.render(
+   <div>
+     <h2>{todoapp}</h2>
+      <h1>"Hello welcome to react"</h1>
+    <h1>"This is my app"</h1>
+    <img src="https://picsum.photos/200/300"/>
+   </div>,
+  document.getElementById('root')
+);
+
+
+
